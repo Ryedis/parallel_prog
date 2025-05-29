fa6e0b8fa7732e58fdb50cf5dfa4667f352672bb
Ryedis <134209766+Ryedis@users.noreply.github.com>
2025-05-29 14:48:52 +0400

    lab 1

---
+++ Lab_1/python_check/graphics.py
@@ -0,0 +1,29 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_data_from_file(filename):
+    x = []
+    y = []
+    with open(filename, 'r') as file:
+        for line in file:
+            data = line.strip().split()
+            x.append(int(data[0]))
+            y.append(int(data[2]))
+    return np.array(x), np.array(y)
+
+filename = 'log.txt'  
+x, y = read_data_from_file(filename)
+
+plt.scatter(x, y, marker='o', color='r', s=10)
+plt.title('График результатов вычислений')  
+plt.xlabel('Размер матрицы n*n') 
+plt.ylabel('Время выполнения (нс)') 
+plt.grid(True)  
+plt.tight_layout()
+plt.savefig(os.path.join('plot.png'), dpi=300)
+plt.show()
+
